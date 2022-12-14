775e95a680d5dd63873042328922d8ff1fc86fd8
Belayneh <bel.zpresent@gmail.com>
2022-12-14 11:02:19 -0800

    Low level programming

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	print_times_table(0);
-	_putchar('\n');
 	print_times_table(3);
 	_putchar('\n');
 	print_times_table(5);
@@ -15,6 +13,5 @@ int main(void)
 	print_times_table(98);
 	_putchar('\n');
 	print_times_table(12);
-
 	return (0);
 }

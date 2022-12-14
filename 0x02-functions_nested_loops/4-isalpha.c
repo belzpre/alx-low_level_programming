a830498ae1962f6d49543f4801da72a2802926ff
Belayneh <bel.zpresent@gmail.com>
2022-12-14 08:23:56 -0800

    Low level programming

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ ** _isalpha - Check description
+ ** @c: An input character
+ ** Description: This checks for alphabetic character.
+ ** Return: 1 if c is alphabet, 0 otherwise.
+ **/
+int _isalpha(int c)
+{
+	char i, j;
+	int m = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (j = 'A'; j <= 'Z'; j++)
+		{
+		       if (c == i || c == j)
+			m = 1;
+		}
+	}
+
+	return (m);
+}

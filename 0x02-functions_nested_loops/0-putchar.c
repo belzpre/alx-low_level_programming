f2e27e9b6033869a7c0c54b1504c95461978d19e
Belayneh <bel.zpresent@gmail.com>
2022-12-14 07:06:34 -0800

    Low level programming

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "fu.h"
 /**
- ** main - see the description
- ** Description: This program prints "_putchar" followed by a new line.
- ** Return: 0
+ **main - see the description
+ **Description: This program prints "_putchar" followed by a new line.
+ **Return:0
  **/
 int main(void)
 {
-	printf("_putchar\n");
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Check description
+ * Description: This program/function prints the alphabet, in lowercase, followed by a new line.
+ * Return: N/A
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}

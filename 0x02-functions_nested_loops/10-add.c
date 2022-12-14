cf01d90a1c986204626fedabed6dec40271c2873
Belayneh <bel.zpresent@gmail.com>
2022-12-14 10:13:38 -0800

    Low level programming

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - See the description
+ * @a: input number a
+ * @b: input number b
+ * Description: This adds two integers and returns the result.
+ * Return: the result of a plus b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}

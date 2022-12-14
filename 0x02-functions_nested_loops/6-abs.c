2c54a43edbfbf4817b8275b1745736523a920401
Belayneh <bel.zpresent@gmail.com>
2022-12-14 09:25:44 -0800

    Low level programming

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - See the description
+ * @r: input number
+ * Description: This gives the absolute value of a number.
+ * Return: absolute value of a number.
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+		return (r * -1);
+}

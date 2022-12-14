01b38cb62967eb43651f0dc8eba04ad074e3f056
Belayneh <bel.zpresent@gmail.com>
2022-12-14 07:57:07 -0800

    Low level programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ ** _islower - check description
+ ** @c: input character
+ ** Description: This prints the alphabet, in lowercase, 10 times,
+ ** followed by a new line.
+ ** Return: 1 if c is lowercase, 0 otherwise.
+ **/
+int _islower(int c)
+{
+	char i;
+	int j = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			j = 1;
+	}
+
+	return (j);
+}

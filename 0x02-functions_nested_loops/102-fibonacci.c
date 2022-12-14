02bddfa9b43559763b1095548fd280e99eb10607
Belayneh <bel.zpresent@gmail.com>
2022-12-14 11:20:10 -0800

    Low level programming

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int f[50];
+
+	f[0] = 1;
+	f[1] = 2;
+	printf("%ld, %ld, ", f[0], f[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		f[i] = f[i - 1] + f[i - 2];
+		if (i == 49)
+			printf("%ld\n", f[i]);
+		else
+			printf("%ld, ", f[i]);
+	}
+
+	return (0);
+}

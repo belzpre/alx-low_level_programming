0b5fe45a103a39e5ee604b6174137eecb6b5f4dd
Belayneh <bel.zpresent@gmail.com>
2022-12-16 15:39:18 -0800

    More funs

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+void print_integer(int m);
+
+/**
+ * print_number - This prints an integer.
+ * @n: Input number.
+ *
+ * Return: N/A.
+ */
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_integer(n * -1);
+	}
+	else
+		print_integer(n);
+}
+
+/**
+ * print_integer - This prints the values of n.
+ * @m: Input number.
+ *
+ * Return: N/A.
+ */
+void print_integer(int m)
+{
+	int i;
+
+	for (i = 100000; i >= 1; i /= 10)
+		if (m / i != 0)
+		{
+			_putchar((m / i) % 10 + '0');
+		}
+}

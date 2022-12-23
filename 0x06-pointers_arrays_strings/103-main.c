5d39fa1a60cdbe9389f8c1484a838fb8d92081c3
Belayneh <bel.zpresent@gmail.com>
2022-12-23 15:02:30 -0800

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/103-main.c
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -17,6 +17,7 @@ int main(void)
 	char *res;
 
 	res = infinite_add(n, m, r, 100);
+
 	if (res == 0)
 	{
 		printf("Error\n");
@@ -25,9 +26,11 @@ int main(void)
 	{
 		printf("%s + %s = %s\n", n, m, res);
 	}
+
 	n = "1234567890";
 	m = "1";
 	res = infinite_add(n, m, r2, 10);
+
 	if (res == 0)
 	{
 		printf("Error\n");
@@ -36,9 +39,11 @@ int main(void)
 	{
 		printf("%s + %s = %s\n", n, m, res);
 	}
+
 	n = "999999999";
 	m = "1";
 	res = infinite_add(n, m, r2, 10);
+
 	if (res == 0)
 	{
 		printf("Error\n");
@@ -47,7 +52,9 @@ int main(void)
 	{
 		printf("%s + %s = %s\n", n, m, res);
 	}
+
 	res = infinite_add(n, m, r3, 11);
+
 	if (res == 0)
 	{
 		printf("Error\n");

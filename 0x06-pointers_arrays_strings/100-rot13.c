9ed1d0869af9d7706a86063abbc73cd0393cc17d
Belayneh <bel.zpresent@gmail.com>
2022-12-23 14:36:42 -0800

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,9 +14,12 @@ char *rot13(char *s)
 		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
 		{
 			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+			{
 				s[i] += 13;
-			else
-				s[i] -= 13;
+				i++;
+				continue;
+			}
+			s[i] -= 13;
 			i++;
 		}
 		i++;

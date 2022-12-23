2c1c5bf6a32d4fc5084be9e14d95ee6c2a4d08a5
Belayneh <bel.zpresent@gmail.com>
2022-12-23 14:05:34 -0800

    Pointers, Arrays and Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,8 @@ char *cap_string(char *s)
 	{
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' &&
+				       	s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
 		i++;
 	}
